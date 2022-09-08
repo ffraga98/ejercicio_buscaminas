43336cb44408e6cd5b1d160cd1e5113c8a762f59
ffraga98 <ferfraga2@gmail.com>
2022-09-08 17:36:47 -0300

    Limpieza del main, ahor aretorna Result

--- buscaminas/src/main.rs
+++ buscaminas/src/main.rs
@@ -1,24 +1,23 @@
-use crate::io::escritor::Escritor;
-use crate::io::lector::Lector;
-use crate::tablero::builder::TableroBuilder;
+use buscaminas::error::error_io::ErrorIO;
+use buscaminas::error::Error;
+use buscaminas::error::Error::Eio;
+use buscaminas::my_io::escritor::Escritor;
+use buscaminas::my_io::lector::Lector;
+use buscaminas::tablero::builder::TableroBuilder;
 use std::env::args;
-
-pub mod error;
-pub mod io;
-pub mod tablero;
 
 const ARCHIVO_SOLUCION: &str = "solucion.txt";
 
-fn main() -> Result<(), ()> {
+fn main() -> Result<(), Error> {
     //DONE: Al invocarse se debe proveer el nombre del archivo.
     //DONE: Modularizacion
     //DONE: Unit Testing
     //TODO: Integration Testing
-    //TODO: Documentacion 
-        //TODO: Cambiar links
-        //TODO: Cambiar ejemplos, que sean ejecutables 
-        //TODO: Cambiar ejemplos, no haya unwraps
-        //TODO: Cambiar ejemplos, no haya unwraps
+    //TODO: Documentacion
+    //TODO: Cambiar links
+    //TODO: Cambiar ejemplos, que sean ejecutables
+    //TODO: Cambiar ejemplos, no haya unwraps
+    //TODO: Cambiar ejemplos, no haya unwraps
     //DONE:   * error
     //TODO: Separar en archivos distintos los errores.
     //TODO:   * io
@@ -30,46 +29,23 @@ fn main() -> Result<(), ()> {
     //TODO:     - coordenada
     //TODO:     - tablero
 
-
     let args: Vec<String> = args().collect();
     if args.len() != 2 {
-        //FIXME: Agregar error
-        println!("Para resolver el problema, se requiere unicamente de la ruta del archivo.");
-        return Err(())
+        return Err(Eio(ErrorIO::ErrorCLI));
     }
     let path = &args[1];
 
     let lector = Lector::new(path);
 
-    let casilleros = match lector.leer_archivo() {
-        Ok(valores) => valores,
-        Err(error) => {
-            println!("{}", error);
-            return Err(())
-        }
-    };
+    let casilleros = lector.leer_archivo()?;
 
-    let builder = match TableroBuilder::new(&casilleros) {
-        Ok(builder) => builder,
-        Err(error) => {
-            println!("{}", error);
-            return Err(())
-        }
-    };
+    let builder = TableroBuilder::new(&casilleros)?;
 
-    let solucion = match builder.crear_tablero() {
-        Ok(tablero) => tablero,
-        Err(error) => {
-            println!("{}", error);
-            return Err(())
-        }
-    };
+    let solucion = builder.crear_tablero()?;
 
     let escritor = Escritor::new(ARCHIVO_SOLUCION);
-    if let Err(error) = escritor.imprimir_item(&solucion) {
-        println!("{}", error);
-        return Err(())
-    }
+
+    escritor.imprimir_item(&solucion)?;
 
     Ok(())
 }
